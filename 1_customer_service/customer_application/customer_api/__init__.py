51ca4b34ebad5b5b6a6ed0aba542ca179b1f1e0e
Hasan <hasanozdemir1@trakya.edu.tr>
2022-02-06 02:33:41 +0300

    Documentation is detailed. Typing Done

---
+++ 1_customer_service/customer_application/customer_api/__init__.py
@@ -0,0 +1,10 @@
+"""
+This script is used to create blueprint object for customer api
+"""
+# path :  root/1_customer_service/customer_application/customer_api/__init.py
+from flask import Blueprint
+
+# create the blueprint object
+c_api_blueprint=Blueprint('customer_api',__name__)
+#  import the routes.py
+from . import routes
