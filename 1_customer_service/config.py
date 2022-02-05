909d85d5439aa9274565a8eb7f49a3f928f67f4a
Hasan <hasanozdemir1@trakya.edu.tr>
2022-02-06 02:01:06 +0300

    Configurations updated

--- 1_customer_service/config.py
+++ 1_customer_service/config.py
@@ -1,28 +1,33 @@
+"""
+This script created to initialize configurations for Customer Microservice
+Hasan Özdemir 02-05-2022
+"""
+# root/1_customer_service/config.py
 
-# config.py
-import os
+from os import path
 from dotenv import load_dotenv
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-if os.path.exists(dotenv_path):
+dotenv_path = path.join(path.dirname(__file__), '.env')
+if path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
-
+# Global Environment Configurations
 class Config:
+    DEBUG=False
+    TESTING=False
     SECRET_KEY = "mrfrIMEngCl0pAKqIIBS_g"
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:Hsn58.34@localhost:5432/burgerzilla_customer'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-
+# Development Environment Configurations
 class DevelopmentConfig(Config):
     ENV = "development"
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://cloudacademy:pfm_2020@host.docker.internal:3306/user_dev'
     SQLALCHEMY_ECHO = True
 
-
+# Production Environment Configurations
 class ProductionConfig(Config):
     ENV = "production"
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://cloudacademy:pfm_2020@user-db:3306/user'
     SQLALCHEMY_ECHO = False
 
