75db16c9c5d2fc9a912c62a11409613f7c87cf79
Hasan <hasanozdemir1@trakya.edu.tr>
2022-02-09 23:37:15 +0300

    Well documented. Comments are more informative

--- 1_customer_service/customer_application/models.py
+++ 1_customer_service/customer_application/models.py
@@ -6,39 +6,41 @@ Hasan Özdemir 02-05-2022
 
 # TODO typing implementation
 from . import db
-from datetime import datetime
+
 
 class Customer(db.Model):
-    """
-    Customer Model
-    """
-    # please look at 3_project_docs/burgerzilla_db_schema.md docs to understand Customer Model
-    __tablename__='Customer'
-    c_id=db.Column(db.Integer, primary_key=True,autoincrement=True)
-    c_email=db.Column(db.String(255), nullable=False)
-    c_full_name=db.Column(db.String(100),nullable=False)
+    """Customer ORM Model definition"""
+
+    # table name declaration
+    __tablename__ = 'Customer'
+    # table columns declaration
+    c_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    c_email = db.Column(db.String(255), nullable=False)
+    c_full_name = db.Column(db.String(100), nullable=False)
     c_address = db.Column(db.String(100), nullable=False)
     c_city = db.Column(db.String(25), nullable=False)
     c_post_code = db.Column(db.Integer, nullable=False)
     c_phone_number = db.Column(db.String(15), nullable=False)
 
-    def __repr__(self)->str:
+    # creates a full string representation of an object
+    def __repr__(self) -> str:
         """
         __repr__ is the special function that returns the string representation of the customer object.
+        The goal of __repr__ is to be unambiguous
         :return: <str> representation of customer object
         """
         return f'<Full Name :{self.c_full_name}>'
 
-    def convert_to_json(self)->dict:
+    def convert_to_json(self) -> dict:
         """
         This method is used to create representable format suitable for JSON based API response
         :return: <dict> JSON based API response
         """
         return {
-            'c_email' : self.c_email,
-            'c_full_name':self.c_full_name,
-            'c_address':self.c_address,
-            'c_city':self.c_city,
-            'c_post_code':self.c_post_code,
-            'c_phone_number':self.c_phone_number
+            'c_email': self.c_email,
+            'c_full_name': self.c_full_name,
+            'c_address': self.c_address,
+            'c_city': self.c_city,
+            'c_post_code': self.c_post_code,
+            'c_phone_number': self.c_phone_number
         }
