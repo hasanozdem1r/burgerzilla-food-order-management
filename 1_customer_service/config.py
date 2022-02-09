9a69d031d753d19e87e60f1cf3636bde127b0a11
Hasan <hasanozdemir1@trakya.edu.tr>
2022-02-09 23:12:16 +0300

    configurations updated. Credentials passed from environment variables

--- 1_customer_service/config.py
+++ 1_customer_service/config.py
@@ -6,23 +6,29 @@ Hasan Özdemir 02-05-2022
 # TODO typing implementation
 from os import path
 from dotenv import load_dotenv
+from utils.helpers import get_postgres_configurations
 
-#dDotenv adds .env support to your flask project.
+# Configuration Variables taken from Windows Environment Variables
+POSTGRES_HOST, POSTGRES_PORT, POSTGRES_USER, POSTGRES_PWD = get_postgres_configurations()
+DB_CONNECTION_URI: str = f'postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PWD}@{POSTGRES_HOST}:' \
+                         f'{POSTGRES_PORT}/burgerzilla_customer '
+
+# dDotenv adds .env support to your flask project.
 # This is the file where we pass the environment variables.
 dotenv_path = path.join(path.dirname(__file__), '.env')
 if path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
+
 # Global Environment Configurations
 # This class contains the configurations common to all environments like development, QA and production, etc.
 class Config:
-    DEBUG=False
-    TESTING=False
-    # generated with secrets library
-    # secrets.url_tokensafe() method is used
+    DEBUG = False
+    TESTING = False
     SECRET_KEY = "mrfrIMEngCl0pAKqIIBS_g"
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:Hsn58.34@localhost:5432/burgerzilla_customer'
+    SQLALCHEMY_DATABASE_URI = DB_CONNECTION_URI
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 # Development Environment Configurations
 # Contains configuration settings used during development.
@@ -31,10 +37,10 @@ class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_ECHO = True
 
+
 # Production Environment Configurations
 # It contains configuration settings being used in production
 class ProductionConfig(Config):
     ENV = "production"
     DEBUG = False
     SQLALCHEMY_ECHO = False
-
