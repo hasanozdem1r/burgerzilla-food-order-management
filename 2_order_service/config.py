e948c78f8044590cd555c390d234659caf8d0dff
Hasan <hasanozdemir1@trakya.edu.tr>
2022-02-09 19:42:42 +0300

    Order microservice project restructuring

---
+++ 2_order_service/config.py
@@ -0,0 +1,44 @@
+"""
+This script created to initialize configurations for Restaurant Microservice
+Hasan Özdemir 02-07-2022
+"""
+# path : root/3_restaurant_service/config.py
+# TODO typing implementation
+from os import path
+from dotenv import load_dotenv
+from helpers import generate_connection_uri,generate_secret_key
+
+
+# dDotenv adds .env support to your flask project.
+# This is the file where we pass the environment variables.
+dotenv_path = path.join(path.dirname(__file__), '.env')
+if path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+
+# Global Environment Configurations
+# This class contains the configurations common to all environments like development, QA and production, etc.
+class Config:
+    DEBUG = False
+    TESTING = False
+    # generated with secrets library
+    # secrets.url_tokensafe() method is used
+    SECRET_KEY = generate_secret_key(32)
+    SQLALCHEMY_DATABASE_URI = generate_connection_uri('db_config.ini')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+# Development Environment Configurations
+# Contains configuration settings used during development.
+class DevelopmentConfig(Config):
+    ENV = "development"
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
+
+
+# Production Environment Configurations
+# It contains configuration settings being used in production
+class ProductionConfig(Config):
+    ENV = "production"
+    DEBUG = False
+    SQLALCHEMY_ECHO = False
