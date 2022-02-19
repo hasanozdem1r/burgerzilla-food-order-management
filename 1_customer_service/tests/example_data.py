6969a6f37a6120261fd893b2b4af9979e8f94e48
Hasan <hasanozdemir1@trakya.edu.tr>
2022-02-19 04:40:52 +0300

    test data created

---
+++ 1_customer_service/tests/example_data.py
@@ -0,0 +1,15 @@
+"""
+This script is created to prepare test data for testing application
+Hasan Özdemir 02-19-2022
+"""
+# path : root/1_customer_service/tests/example_data.py
+
+valid_customer_data = {
+    "c_id": 1,
+    "c_email": "hasan@hasan.com",
+    "c_full_name": "Hasan Ozdemir",
+    "c_address": "XYZ, ABC",
+    "c_city": "Dublin",
+    "c_postal_code": 12345,
+    "c_phone_number": "+123456789",
+}
