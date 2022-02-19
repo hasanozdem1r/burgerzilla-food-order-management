f4e12e0f7f5f6b10201c0e5ecf7297e2ae333eb0
Hasan <hasanozdemir1@trakya.edu.tr>
2022-02-19 04:39:50 +0300

    Validators updated.

--- 1_customer_service/customer_application/model_validation.py
+++ 1_customer_service/customer_application/model_validation.py
@@ -1,14 +1,20 @@
-from models import CustomerOrm
-from pydantic import BaseModel,Field
+"""
+This script is used to validate models
+Hasan Özdemir 02-19-2022
+"""
+# path : # path : root/1_customer_service/customer_application/model_validation.py
+from pydantic import BaseModel, Field
 
 
 class Customer(BaseModel):
-    c_email:str=Field(...,required=True,description="Email",regex="^.+@.+$")
-    c_full_name:str=Field(...,required=True,description="Full Name",regex="\w*\d*\s*")
-    c_address:str=Field(...,required=True,description="Address")
-    c_city:str
-    c_postal_code:int
-    c_phone_number:str
+    c_email: str = Field(..., required=True, description="Email", regex="^.+@.+$")
+    c_full_name: str = Field(
+        ..., required=True, description="Full Name", regex="\w*\d*\s*"
+    )
+    c_address: str = Field(..., required=True, description="Address")
+    c_city: str
+    c_postal_code: int
+    c_phone_number: str
 
     # the Config class is used to provide configurations to Pydantic
     class Config:
@@ -16,15 +22,14 @@ class Customer(BaseModel):
         orm_mode = True
 
 
-
-if __name__=='__main__':
-    customer_obj=CustomerOrm(
-        c_email="hasan@hasan.com",
-        c_full_name="Hasan Ozdemir",
-        c_address="XYZ Str, KLM",
-        c_city="KLM",
-        c_postal_code=12345,
-        c_phone_number="0123456789"
-    )
-    if Customer.from_orm(customer_obj):
-        print("VALID")
+if __name__ == "__main__":
+    customer_obj = {
+        "c_email":"hasan@hasan.com",
+        "c_full_name":"Hasan Ozdemir",
+        "c_address":"XYZ Str, KLM",
+        "c_city":"KLM",
+        "c_postal_code":12345,
+        "c_phone_number":"0123456789",
+    }
+    if Customer(**customer_obj):
+        pass
